ce1887babe73b368f213fd3af3171542b5732c17
raysong <raysong@sina.com>
2025-02-13 18:17:25 +0800

    白盒：https://github.com/q215613905/TVBoxOS

---
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/HomeHotVodAdapter.java
@@ -0,0 +1,94 @@
+package com.github.tvbox.osc.ui.adapter;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.text.TextUtils;
+import android.util.Base64;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.github.tvbox.osc.R;
+import com.github.tvbox.osc.api.ApiConfig;
+import com.github.tvbox.osc.bean.Movie;
+import com.github.tvbox.osc.picasso.RoundTransformation;
+import com.github.tvbox.osc.util.DefaultConfig;
+import com.github.tvbox.osc.util.HawkConfig;
+import com.github.tvbox.osc.util.MD5;
+import com.orhanobut.hawk.Hawk;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import me.jessyan.autosize.utils.AutoSizeUtils;
+
+public class HomeHotVodAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHolder> {
+
+    public HomeHotVodAdapter() {
+        super(R.layout.item_user_hot_vod, new ArrayList<>());
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, Movie.Video item) {
+    	// takagen99: Add Delete Mode
+        FrameLayout tvDel = helper.getView(R.id.delFrameLayout);
+        if (HawkConfig.hotVodDelete) {
+            tvDel.setVisibility(View.VISIBLE);
+        } else {
+            tvDel.setVisibility(View.GONE);
+        }
+
+        TextView tvRate = helper.getView(R.id.tvRate);
+        if (Hawk.get(HawkConfig.HOME_REC, 0) == 2){
+            tvRate.setText(ApiConfig.get().getSource(item.sourceKey).getName());
+        }else if(Hawk.get(HawkConfig.HOME_REC, 0) == 0){
+            tvRate.setText("豆瓣热播");
+        }else {
+            tvRate.setVisibility(View.GONE);
+        }
+
+        TextView tvNote = helper.getView(R.id.tvNote);
+        if (item.note == null || item.note.isEmpty()) {
+            tvNote.setVisibility(View.GONE);
+        } else {
+            tvNote.setText(item.note);
+            tvNote.setVisibility(View.VISIBLE);
+        }
+        helper.setText(R.id.tvName, item.name);
+        ImageView ivThumb = helper.getView(R.id.ivThumb);
+        //由于部分电视机使用glide报错
+        if (!TextUtils.isEmpty(item.pic)) {
+            item.pic=item.pic.trim();
+            if(isBase64Image(item.pic)){
+                // 如果是 Base64 图片，解码并设置
+                ivThumb.setImageBitmap(decodeBase64ToBitmap(item.pic));
+            }else {
+                Picasso.get()
+                        .load(DefaultConfig.checkReplaceProxy(item.pic))
+                        .transform(new RoundTransformation(MD5.string2MD5(item.pic))
+                                .centerCorp(true)
+                                .override(AutoSizeUtils.mm2px(mContext, 300), AutoSizeUtils.mm2px(mContext, 400))
+                                .roundRadius(AutoSizeUtils.mm2px(mContext, 10), RoundTransformation.RoundType.ALL))
+                        .placeholder(R.drawable.img_loading_placeholder)
+                        .noFade()
+                        .error(R.drawable.img_loading_placeholder)
+                        .into(ivThumb);
+            }
+        } else {
+            ivThumb.setImageResource(R.drawable.img_loading_placeholder);
+        }
+    }
+    private boolean isBase64Image(String picUrl) {
+        return picUrl.startsWith("data:image");
+    }
+
+    private Bitmap decodeBase64ToBitmap(String base64Str) {
+        // 去掉 Base64 数据的头部前缀，例如 "data:image/png;base64,"
+        String base64Data = base64Str.substring(base64Str.indexOf(",") + 1);
+        byte[] decodedBytes = Base64.decode(base64Data, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
+    }
+}
